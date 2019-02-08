62cd2b70cbd666e4bb96b2e50371015ca1ec78c7
Ben Arent <benarent@gmail.com>
2019-02-08 13:49:24 -0800

    Set user

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 bundle install > /dev/null 2>&1
 bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
+RUN chown -R jekyll /Users
 bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO S3'
 s3_website push
