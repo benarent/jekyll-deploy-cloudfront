f05f0ebec60729303d64e9c590407f3aacc83a8b
Ben Arent <benarent@gmail.com>
2019-02-09 09:25:36 -0800

    Cleans the yokes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 bundle install > /dev/null 2>&1
 bundle list | grep "jekyll ("
-echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
-bundle exec jekyll build
+echo 'Cleaning the yokes. '
 bundle exec jekyll clean
+echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO S3'
 s3_website push
