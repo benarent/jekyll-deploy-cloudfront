639d2c2adc7c34225de152ad1f46a5f86b83ef67
Ben Arent <benarent@gmail.com>
2019-02-09 08:51:35 -0800

    rinse and repeat

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ bundle install > /dev/null 2>&1
 bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 bundle exec jekyll build
+bundle exec jekyll clean
+bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO S3'
 s3_website push
 echo '👍 GREAT SUCCESS!'
